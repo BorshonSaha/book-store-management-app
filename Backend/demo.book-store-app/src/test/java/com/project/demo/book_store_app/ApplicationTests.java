750b14c9f44e50e1e3eff98a949588c0671f91fd
Borshon Saha <I-588@wgbd.com>
2024-11-18 10:21:26 +0600

    project setup done

---
+++ Backend/demo.book-store-app/src/test/java/com/project/demo/book_store_app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.demo.book_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
